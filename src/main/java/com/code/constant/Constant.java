1e848c9eb6059d8b2f18bea5cd6648061e81abfc
kay <kay.58920@qq.com>
2021-04-10 20:47:57 +0800

    test

---
+++ src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,32 @@
+package com.code.constant;
+
+/**
+ * Created by dm.
+ */
+public class Constant {
+
+    public static String template_path = "D:\\work\\work-demo\\code-parent\\src\\main\\resources\\template";
+
+    //实体model类生成地址
+    public static String model_save_path = "D:\\work\\work-demo\\common\\cateyes-common\\cateyes-common-module\\src\\main\\java\\com\\cateyes\\entity";
+
+    public static String mapper_save_path = "D:\\work\\work-demo\\common\\cateyes-common\\cateyes-common-dao\\src\\main\\resources\\mapper";
+    public static String class_mapper_save_path = "D:\\work\\work-demo\\common\\cateyes-common\\cateyes-common-dao\\src\\main\\java\\com\\cateyes\\mapper";
+
+    public static String client_dm_save_path = "D:\\work\\work-demo\\common\\cateyes-common\\cateyes-common-client\\src\\main\\java\\com\\cateyes\\client";
+    public static String client_dm_fallback_save_path = "D:\\work\\work-demo\\common\\cateyes-common\\cateyes-common-client\\src\\main\\java\\com\\cateyes\\fallback";
+    //item provider路径
+    public static String service_dm_item_save_path = "D:\\work\\work-demo\\cateyes-item-provider\\src\\main\\java\\com\\cateyes\\service";
+    //order provider路径
+    public static String service_dm_order_save_path = "D:\\work\\work-demo\\cateyes-order-provider\\src\\main\\java\\com\\cateyes\\service";
+    //pay provider路径
+    public static String service_dm_pay_save_path = "D:\\work\\work-demo\\cateyes-pay-provider\\src\\main\\java\\com\\cateyes\\service";
+    //scheduler provider路径
+    public static String service_dm_scheduler_save_path = "D:\\work\\work-demo\\cateyes-scheduler-provider\\src\\main\\java\\com\\cateyes\\service";
+    //user provider路径
+    public static String service_dm_user_save_path = "D:\\work\\work-demo\\cateyes-user-provider\\src\\main\\java\\com\\cateyes\\service";
+    //common provider路径
+    public static String service_dm_common_save_path = "D:\\work\\work-demo\\cateyes-base-provider\\src\\main\\java\\com\\cateyes\\service";
+
+
+}
